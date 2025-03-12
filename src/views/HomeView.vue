c0648fad7428c0be913fc0bc165df4e0f356a236
AxelDAW <axelfreire18@gmail.com>
2025-03-12 08:48:13 +0100

    Pimer commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <TheWelcome />
+
+</template>
+
+<script>
+
+import TheWelcome from '../components/TheWelcome.vue'
+
+export default {
+
+    name: 'HomeView',
+
+    components: { TheWelcome }
+
+}
+
+</script>
